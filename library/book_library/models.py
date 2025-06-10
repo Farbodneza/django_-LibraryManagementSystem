d82be5defabf77fe7971879d8399744c11bd4ca2
Farbod Nezafati <farbodneza@gmail.com>
2025-06-10 14:53:56 +0330

    refactor(models): use title for slug in Book model and refactor author model

--- library/book_library/models.py
+++ library/book_library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.utils.text import slugify
 
 class Member(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True)
@@ -9,8 +10,13 @@ class Member(AbstractUser):
         return self.username
 
 
-class Author(Member):
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Book(models.Model):
@@ -19,9 +25,18 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_returned = models.BooleanField(default=True)
     description = models.TextField(null=True, blank=True)
-    slug = models.SlugField(max_length=30)
+    slug = models.SlugField(max_length=30, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title[:30])
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ["title"]
+    
+    def __str__(self):
+        return self.title
 
 
 class BorrowedBook(models.Model):
